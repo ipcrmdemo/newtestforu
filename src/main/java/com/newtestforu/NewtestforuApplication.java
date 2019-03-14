bc66aa7b1a77dd90be0066ac627bd55498549db5
Matt Cadorette <mcadorette@gmail.com>
2019-03-14 07:55:25 -0400

    Initial commit from Atomist
    [atomist:generated] [atomist:generator=create-spring]

---
+++ src/main/java/com/newtestforu/NewtestforuApplication.java
@@ -0,0 +1,12 @@
+package com.newtestforu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewtestforuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewtestforuApplication.class, args);
+	}
+}
